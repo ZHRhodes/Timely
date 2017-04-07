fd6f10a946fde3d4f975c4b2decbe49a9ae0b57c
Zack Rhodes <zhrhodes@gmail.com>
2017-04-06 22:26:38 -0500

    Initial Commit

---
+++ Timely/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Timely
+//
+//  Created by Zack Rhodes on 4/6/17.
+//  Copyright © 2017 zhrhodes. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
